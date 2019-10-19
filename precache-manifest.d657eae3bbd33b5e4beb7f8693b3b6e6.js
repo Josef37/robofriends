5ad4afd13d987d08f3fbcd82204fb837885df520
Josef Wittmann <josefwittmann42@gmail.com>
2019-10-19 12:22:27 +0200

    Updates

---
+++ precache-manifest.d657eae3bbd33b5e4beb7f8693b3b6e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "370e25caaaac8c2c06e74b887183fa2a",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "52a17258f9db6358049c",
+    "url": "/robofriends/static/css/main.15deac6c.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "52a17258f9db6358049c",
+    "url": "/robofriends/static/js/main.0e0aa7fa.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
