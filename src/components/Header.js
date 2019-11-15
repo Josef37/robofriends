5b099f5f64ff7f40026cb41ea1a1d4b36429f26c
Josef Wittmann <josefwittmann42@gmail.com>
2019-11-15 09:36:31 +0100

    Added Coutner Button to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, Fragment } from "react";
+import CounterButton from "./CounterButton";
 
 class Header extends PureComponent {
   render() {
-    return <h1 className="f1">RoboFriends</h1>;
+    return (
+      <Fragment>
+        <h1 className="f1">RoboFriends</h1>
+        <CounterButton />
+      </Fragment>
+    );
   }
 }
 
